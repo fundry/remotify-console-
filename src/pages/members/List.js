6f402ca894b617a491c2a8a7b41435bb95162af4
Nwani Victory <vickywane@gmail.com>
2019-11-22 10:33:53 +0100

    Expand  members

--- src/pages/members/List.js
+++ src/pages/members/List.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Flex from "styled-flex-component";
 import styled from "styled-components";
 import { Image } from "react-bootstrap";
 import posed from "react-pose";
+import { IoMdSettings, IoMdClose, IoMdSearch } from "react-icons/io";
+import media from "styled-media-query";
+
+import useWindowWidth from "../../hooks_style";
 
 const List = () => {
-  const Body = styled.div`
-    padding: 0.7;
-  `;
+  const hooks = useWindowWidth();
 
   const autoGrid = (minColumnWidth = 150, gridGap = 0) => ({
     display: "grid",
@@ -23,6 +25,19 @@ const List = () => {
     justifyContent: "center",
     alignContent: "center"
   });
+
+  const Data = [
+    { name: "Mane Mane" },
+    { name: "Tane Tane" },
+    { name: "Rane  Rane" },
+    { name: "Fane Fane" },
+    { name: "Gane Gane" },
+    { name: "Vane Vane" },
+    { name: "Dane Dane" },
+    { name: "Nane Nane" },
+    { name: "Wane Nane" },
+    { name: "Zane Zane" }
+  ];
 
   const Bounce = posed.div({
     hoverable: true,
@@ -40,41 +55,144 @@ const List = () => {
     }
   });
 
-  const Data = [
-    { name: "" },
-    { name: "" },
-    { name: "" },
-    { name: "" },
-    { name: "" },
-    { name: "" },
-    { name: "" },
-    { name: "" },
-    { name: "" },
-    { name: "" }
-  ];
+  const Input = styled.input({
+    width: "25em",
+    height: "5vh",
+    border: "0px",
+    borderRadius: "0px",
+    paddingLeft: "10px",
+    marginBottom: "15px"
+  });
+
+  const Box = styled.div({
+    width: "28em",
+    height: "6.5vh",
+    border: "1px solid #000",
+    borderRadius: "5px",
+    padding: "0.5em"
+  });
+
+  const Body = styled.div`
+    padding: 0.7em;
+  `;
+
+  const Hover = styled.div`
+    cursor: pointer;
+  `;
+
+  const Name = styled.p`
+    font-size : 1.1em
+    padding-left : 20px
+    padding-top : 15px
+    word-spacing : 1em
+  `;
+
+  const DeptName = styled.h3`
+    padding-left: 17px
+    padding-top: 10px
+  ${media.lessThan("medium")`
+   font-size: 1.2em
+   padding-left: 14px
+  `};
+  `;
+
+  const [Search, SetSearch] = useState(false);
+  const [SmallSearch, SetSmallSearch] = useState(false);
 
   return (
     <Body>
-      <h4 style={{ textAlign: "center" }}> Workers </h4>
       <Body>
-        <h5 style={{ textAlign: "left", paddingLeft: "17px" }}>Engineering </h5>
-        <hr />
-        <Images>
-          {Data.map(({ i }) => {
-            return (
-              <div>
-                <Bounce key={i}>
-                  <Image
-                    src={require("./sample.png")}
-                    style={{ height: "77px" }}
-                    roundedCircle
+        <Flex justifyBetween>
+          {!SmallSearch ? <DeptName>Engineering</DeptName> : null}
+
+          {hooks >= 650 ? (
+            <Box>
+              <Flex justifyBetween>
+                <Hover style={{ paddingTop: "7px", paddingLeft: "7px" }}>
+                  <IoMdSearch style={{ fontSize: "1.8em" }} />
+                </Hover>
+                <Input placeholder="Search Worker Name" />
+              </Flex>
+            </Box>
+          ) : (
+            <div>
+              {!SmallSearch ? (
+                <Hover style={{ paddingRight: "10px" }}>
+                  <IoMdSearch
+                    style={{ fontSize: "1.5em" }}
+                    onClick={() => {
+                      SetSmallSearch(true);
+                    }}
                   />
-                </Bounce>
-              </div>
+                </Hover>
+              ) : (
+                <Box
+                  style={{
+                    width: "25em",
+                    height: "5vh",
+                    padding: "0.1em"
+                  }}
+                >
+                  <Flex justifyBetween>
+                    <Hover style={{ paddingTop: "7px", paddingLeft: "7px" }}>
+                      <IoMdClose
+                        style={{ fontSize: "1.8em" }}
+                        onClick={() => {
+                          SetSmallSearch(false);
+                        }}
+                      />
+                    </Hover>
+                    <Input
+                      placeholder="Search Worker Name"
+                      style={{
+                        width: "23em",
+                        height: "4vh"
+                      }}
+                    />
+                  </Flex>
+                </Box>
+              )}
+            </div>
+          )}
+        </Flex>
+
+        <hr />
+        <div>
+          {Data.map(({ i, name }) => {
+            return (
+              <Body>
+                <Flex justifyBetween>
+                  <Flex>
+                    <Image
+                      src={require("./sample.png")}
+                      style={{ height: "50px" }}
+                      roundedCircle
+                    />
+
+                    <Name> {name} </Name>
+                  </Flex>
+
+                  <Flex>
+                    <Hover>
+                      <IoMdSettings style={{ fontSize: "1.5em" }} />
+                    </Hover>
+
+                    <Hover style={{ paddingLeft: "15px" }}>
+                      <IoMdSettings style={{ fontSize: "1.5em" }} />
+                    </Hover>
+
+                    <Hover style={{ paddingLeft: "15px" }}>
+                      <IoMdSettings style={{ fontSize: "1.5em" }} />
+                    </Hover>
+                  </Flex>
+                </Flex>
+              </Body>
             );
           })}
-        </Images>{" "}
-      </Body>{" "}
+        </div>
+        <br />
+      </Body>
+
       <Body>
         <h5 style={{ textAlign: "left", paddingLeft: "17px" }}>Engineering </h5>
         <hr />
--- src/pages/members/info.tsx
+++ src/pages/members/info.tsx
@@ -1,55 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Flex from "styled-flex-component";
+import media from "styled-media-query";
+import useWindowWidth from "../../hooks_style";
 
 const Info = (): JSX.Element => {
+  const hook: number = useWindowWidth();
+
   const Body = styled.div`
     padding: 1em;
   `;
 
-  const Button = styled.button`
+  const CurvedButton = styled.button`
     background: #361f94
-    height: 70px;
+    height: 60px;
     border: 0px;
     border-radius: 0px 15px 15px 0px;
     color: #fff;
-    margin: 0 1em;
+    margin: 0em 1em ;
     padding: 1.2em 1.77em;
     font-size: 1.1em;
     &:hover {
-      color: #361f94;
+      color: #fff;
     }
   `;
 
-  const Input = styled.input({
-    padding: "1em",
-    paddingLeft: "2em",
-    fontSize: "1.2em",
-    width: "25em",
-    border: "0px",
-    background: "transparent"
-  });
+  const Button = styled.button`
+    background: #361f94
+    height: 50px;
+    border: 0px;
+    border-radius: 5px;
+    color: #fff;
+    margin: 0 1em;
+    padding: 0.70em 1.50em;
+    font-size: 1.1em;
 
-  const Box = styled.div({
-    border: "1px solid #361f94 ",
-    borderRadius: "0px 15px 15px 0px",
-    height: "7.4vh"
-  });
+    &:hover {
+      color: #fff;
+    }
+  `;
+
+  const Input = styled.input`
+    padding: 1em
+    padding-left: 1em
+    font-size: 1.1em
+    width: 30em
+    height : 5vh
+    border: 0px
+    margin-top :10px
+    background: transparent
+  `;
+
+  const Box = styled.div`
+    border: 1px solid #361f94
+    border-radius: 0px 15px 15px 0px
+    height: 7.4vh
+    width: 32em
+   `;
+
+  const [Invite, SetInvite] = useState<boolean>(false);
+  const [Notice, SetNotice] = useState<boolean>(false);
 
   return (
     <Body>
-      <p> info </p>
+      {!Invite ? (
+        <Flex justifyCenter>
+          <Button
+            onClick={() => {
+              SetInvite(true);
+            }}
+          >
+            Invite
+          </Button>
+        </Flex>
+      ) : (
+        <Flex justifyCenter>
+          <Box>
+            <Flex justifyBetween>
+              <Input placeholder="Invite Staff" />
 
-      <Flex justifyCenter>
-        <Box>
-          <Flex justifyBetween>
-            <Input placeholder="Invite Staff" />
-
-            <Button style={{ cursor: "pointer" }}> Invite </Button>
-          </Flex>
-        </Box>
-      </Flex>
-
+              <CurvedButton style={{ cursor: "pointer" }}>Invite </CurvedButton>
+            </Flex>
+          </Box>
+        </Flex>
+      )}
       <hr />
     </Body>
   );
